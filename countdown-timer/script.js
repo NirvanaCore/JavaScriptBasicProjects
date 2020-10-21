f854bd2d18f8c0495834837e52ef608185f46312
NirvanaCore <nirvanacore7@gmail.com>
2020-10-21 10:39:07 +0530

    it will update year when its new year timer ends

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -2,13 +2,15 @@ const daysCount = document.getElementById('days');
 const hoursCount = document.getElementById('hours');
 const minutesCount = document.getElementById('minutes');
 const secondsCount = document.getElementById('seconds');
-
-const newYears = "1 Jan 2021";
+const newYearCount =document.getElementById('year');
 
 function countDown( ){
   
   const currentDate = new Date();
-  const newYearDate = new Date(newYears);  
+
+  const newYears = ((currentDate.getFullYear())+1);
+
+  const newYearDate = new Date(`Jan 1, ${newYears}`); 
 
   const totalSeconds = (newYearDate- currentDate)/1000;
 
@@ -16,11 +18,13 @@ function countDown( ){
   const hours = Math.floor(totalSeconds / 3600) % 24;
   const minutes = Math.floor(totalSeconds / 60) % 60;
   const seconds =Math.floor(totalSeconds) %60;
+  const year = Math.floor(newYears);
 
   daysCount.innerHTML = formateTime(days);
   hoursCount.innerHTML = formateTime(hours);
   minutesCount.innerHTML = formateTime(minutes);
   secondsCount.innerHTML = formateTime(seconds);
+  newYearCount.innerHTML = formateTime(year);
 
 }
 
@@ -28,7 +32,6 @@ function formateTime(time) {
   return time <10 ? `0${time}` : time;}
 
 // initial call
-
 countDown();
 
 setInterval(countDown,1000);
